af844faf2849e7a9e84f63b3966453b4ada10b33
Andre Freeman <asf@mail.usf.edu>
2020-06-09 18:02:23 -0400

    Created Delete Endpoint

--- users-post.js
+++ users-post.js
@@ -1,10 +1,12 @@
 const express = require("express")
-const users = require("./data/db")
+const posts = require("./data/db")
 const PostRouter = express.Router()
+
+//GET ENDPOINTS
 
 PostRouter.get("/posts", (req, res) => {
 	console.log(req.query)
-	users.find({})
+	posts.find({})
 		.then((post) => {
 			res.status(200).json(post)
 		})
@@ -17,7 +19,7 @@ PostRouter.get("/posts", (req, res) => {
 })
 
 PostRouter.get("/posts/:id", (req, res) => {
-	users.findById(req.params.id)
+	posts.findById(req.params.id)
 		.then((post) => {
 			if (post) {
 				res.status(200).json(post)
@@ -36,14 +38,14 @@ PostRouter.get("/posts/:id", (req, res) => {
 })
 
 PostRouter.get("/posts/:id/comments", (req, res) => {
-    users.findById(req.params.id)
+    posts.findById(req.params.id)
     .then ((post) => {
     if (!post) {
         return res.status(404).json({
             message: "User not found"
         })
     } else {
-    return users.findPostComments(req.params.id) }
+    return posts.findPostComments(req.params.id) }
     
 })
 .then((comments) => {
@@ -57,5 +59,74 @@ PostRouter.get("/posts/:id/comments", (req, res) => {
     })
 })
 })
+
+//POST ENDPOINTS
+
+PostRouter.post("/posts", (req, res) => {
+	if (!req.body.title || !req.body.contents) {
+		return res.status(400).json({
+			message: "Missing user name or email",
+		})
+	}
+
+	posts.insert(req.body)
+		.then((post) => {
+			res.status(201).json(post)
+		})
+		.catch((error) => {
+			console.log(error)
+			res.status(500).json({
+				message: "Error adding the user",
+			})
+		})
+})
+
+PostRouter.post("/posts/:id/comments", (req, res) => {
+    posts.findCommentById(req.params.id)
+    .then ((post) => {
+    if (!post) {
+        return res.status(404).json({
+            message: "User not found"
+        })
+    } else {
+    return posts.insertComment(req.body) }
+    
+})
+.then((comment) => {
+    res.status(201).json(comment)
+
+})
+.catch((error) => {
+    console.log(error)
+    res.status(500).json({
+        message: "Error getting the user",
+    })
+})
+})
+
+// DELETE ENDPOINT
+
+PostRouter.delete("/posts/:id", (req, res) => {
+	posts.remove(req.params.id)
+		.then((count) => {
+			if (count > 0) {
+				res.status(200).json({
+					message: "The user has been nuked",
+				})
+			} else {
+				res.status(404).json({
+					message: "The user could not be found",
+				})
+			}
+		})
+		.catch((error) => {
+			console.log(error)
+			res.status(500).json({
+				message: "Error removing the user",
+			})
+        })
+    })
+
+
 
 module.exports = PostRouter
