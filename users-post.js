d77ff53d88ae930051cf61f854170212dbca176b
Andre Freeman <asf@mail.usf.edu>
2020-08-11 18:24:29 -0400

    Created POST new post

--- users-post.js
+++ users-post.js
@@ -66,4 +66,24 @@ PostRouter.get("/posts", (req, res) => {
       });
   });
 
+  PostRouter.post("/posts", (req, res) => {
+    if (!req.body.title || !req.body.contents) {
+      return res.status(400).json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+    }
+  
+    posts
+      .insert(req.body)
+      .then(post => {
+        res.status(201).json(post)
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  });
+});
+
 module.exports = PostRouter;
