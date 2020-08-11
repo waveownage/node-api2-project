43d7975121c93d2ee04d11a987355bcb702f678b
Andre Freeman <asf@mail.usf.edu>
2020-08-11 17:52:33 -0400

    Created Index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const PostRouter = require("./users-post")
+
+const server = express()
+const port = 8080
+
+server.use(express.json())
+server.use(PostRouter)
+
+server.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`)
+})
