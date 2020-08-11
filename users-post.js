cf447f6d2c34520642a5bdc3c41ee5ca694ba170
Andre Freeman <asf@mail.usf.edu>
2020-08-11 18:14:53 -0400

    Created GET posts by ID endpoint

--- users-post.js
+++ users-post.js
@@ -19,4 +19,26 @@ PostRouter.get("/posts", (req, res) => {
       });
   });
 
+  //GET posts by ID
+
+  PostRouter.get("/posts/:id", (req, res) => {
+    posts
+      .findById(req.params.id)
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          error: "The post information could not be retrieved."
+        });
+      });
+  });
+
 module.exports = PostRouter;
